1023c7c18e5fc1c03e8fc488bdc5c760826db1bf
Joseph Abero <joseph.abero@yahoo.com>
2019-10-14 06:02:57 +0100

    Added THROTTLE test

--- test_scripts/testTHROTTLE.py
+++ test_scripts/testTHROTTLE.py
@@ -4,6 +4,7 @@
 import sys
 import RPi.GPIO as GPIO
 from time import sleep
+from numpy import arange
 
 # print sys.argv[2]
 
@@ -15,24 +16,47 @@ ledpin = 12			# PWM pin connected to LED
 GPIO.setwarnings(False)			#disable warnings
 GPIO.setmode(GPIO.BCM)		#set pin numbering system
 GPIO.setup(ledpin,GPIO.OUT)
-pi_pwm = GPIO.PWM(ledpin,100)		#create PWM instance with frequency
+pi_pwm = GPIO.PWM(ledpin,50)		#create PWM instance with frequency
 pi_pwm.start(0)				#start PWM of required Duty Cycle 
 
 while True:
-    print("--------")
-    print("0 - 100")
-    for duty in range(0,100,1):
-        print(duty)
-        pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
-        sleep(0.01)
-    sleep(1)  
-    print("100 - 0")
-    print("--------")
-    for duty in range(100,-1,-1):
-        print(duty)
-        pi_pwm.ChangeDutyCycle(duty)
-        sleep(0.01)
-    sleep(1)
+    # print("--------")
+    # print("0 - 100")
+    # for duty in range(0,10,1):
+        # print(duty)
+        # pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
+        # sleep(0.01)
+    # sleep(1)  
+    # print("forward")
+    # for duty in arange(7, 8, .25):
+        # pi_pwm.ChangeDutyCycle(duty)
+        # sleep(0.01)
+    # sleep(5)
+    print("forward")
+    pi_pwm.ChangeDutyCycle(7.65)
+    # for duty in arange(8, 7.4, -.25):
+        # print(duty)
+        # pi_pwm.ChangeDutyCycle(duty)
+        # sleep(0.01)
+    sleep(5)
+    print("stop")
+    pi_pwm.ChangeDutyCycle(7)
+    # for duty in arange(7.5, 6.9, -.25):
+        # print(duty)
+        # pi_pwm.ChangeDutyCycle(duty)
+        # sleep(0.01)
+    sleep(5)
+    print("back")
+    pi_pwm.ChangeDutyCycle(5.75)
+    sleep(5)
+    # print("100 - 0")
+    # print("--------")
+    # for duty in range(10,-1,-1):
+        # print(duty)
+        # pi_pwm.ChangeDutyCycle(duty)
+        # sleep(0.01)
+    # sleep(1)
+    
     
     
     # print("0")
